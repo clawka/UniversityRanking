308dc7d39241be10d545cc57efcd6c850568ed98
Stsiapan Yakaukin <stepa.yakovkin@gmail.com>
2025-01-17 13:58:50 +0100

    First commit

---
+++ UniversityRanking/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UniversityRanking.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
